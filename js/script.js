e3f64f661fafedba105f1e699a0fdcc85c200fb7
kingakowalska7 <117362742+kingakowalska7@users.noreply.github.com>
2022-12-12 15:07:44 +0100

    zadanie

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
 $(document).ready(function() {
-    
-$('#dane-programisty').click(function() {
 
-$.get('https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php')
-.done(function (data) {
-    let jqBody = $('body');
-    jqBody.append(data);
-    console.log(data);
-   });      
- });
+$('#dane-programisty').on('click', function() {
+
+$.getJSON('https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php', function(data) 
+{
+$('<p>').text('Imie: ' + data.imie).appendTo('body');
+$('<p>').text('Nazwisko: ' + data.nazwisko).appendTo('body');
+$('<p>').text('Zawod: ' + data.zawod).appendTo('body');
+$('<p>').text('Firma: ' + data.firma).appendTo('body');
+$('<hr>').appendTo('body');
 });
+});
+ });
